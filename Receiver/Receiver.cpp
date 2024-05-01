43cd5579ec4f99494d140fd06f47d28da4da22ef
ThibeVanOrshaegen <thibe.van@hotmail.com>
2024-05-01 15:34:48 +0200

    Above 3 or not

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -5,131 +5,116 @@
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
-#include <QObject>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QEventLoop>
 
-#ifndef _WIN32
-#include <unistd.h>
-#else
-#include <windows.h>
-#define sleep(n)    Sleep(n)
-#endif
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-    try
-    {
+
+    try {
         zmq::context_t context(1);
 
-        // Outgoing message go out through here
+        // Outgoing messages go out through here
         zmq::socket_t ventilator(context, ZMQ_PUSH);
         ventilator.connect("tcp://benternet.pxl-ea-ict.be:24041");
 
         // Incoming messages come in here
         zmq::socket_t subscriber(context, ZMQ_SUB);
         subscriber.connect("tcp://benternet.pxl-ea-ict.be:24042");
-        subscriber.setsockopt(ZMQ_SUBSCRIBE, "Stock>quest?>", 13);
+        subscriber.setsockopt(ZMQ_SUBSCRIBE, "thibe>quest?>", 13); // Subscribe to all messages
 
+        // Receive message
         zmq::message_t msg;
-        subscriber.recv(&msg);
+        subscriber.recv(msg);
+        std::string request = std::string(static_cast<char*>(msg.data()), msg.size());
+        std::cout << "Received: " << request << std::endl;
 
-        QString StockCADL = "Stock>quest?>CADL";
-        QString StockAAPL = "Stock>quest?>AAPL";
-        QString StockCADLGROUPED = "Stock>quest?>CADLPrevious";
-        QString StockAAPLGROUPED = "Stock>quest?>PreviousAAPL";
+        // Make HTTP request to API
+        QNetworkAccessManager manager;
+        QNetworkRequest networkRequest(QUrl("https://api.polygon.io/v1/open-close/CADL/2023-01-09?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS"));
 
+        QEventLoop loop;
+        QObject::connect(&manager, &QNetworkAccessManager::finished, &loop, &QEventLoop::quit);
+        QNetworkReply* reply = manager.get(networkRequest);
+        loop.exec();
 
-        QString receivedMessage = QString::fromStdString(std::string(static_cast<char*>(msg.data()), msg.size()));
-        if (receivedMessage == "Stock>quest?>CADL")
-        {
-            // Send the appropriate message to the ventilator
-            ventilator.send("Stock>quest?>CADL", 19);
+        if (reply->error() == QNetworkReply::NoError) {
+            // Read API response
+            QByteArray responseData = reply->readAll();
+            QJsonDocument jsonResponse = QJsonDocument::fromJson(responseData);
+            QJsonObject jsonObject = jsonResponse.object();
 
-            // Make the HTTP request
-            QNetworkAccessManager networkManager;
-            QNetworkRequest request(QUrl("https://api.polygon.io/v1/open-close/CADL/2023-01-09?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS"));
-            QNetworkReply *reply = networkManager.get(request);
+            // Extract the "high" value from the JSON object
+            double highValue = jsonObject["high"].toDouble();
+            std::cout << "High Value: " << highValue << std::endl;
 
-            // Connect to the finished signal of the network reply
-            QObject::connect(reply, &QNetworkReply::finished, [=, &ventilator]() {
+            if(highValue > 3.0)
+            {
+                std::cout << "High Value is under 3"<<std::endl;
+
+                // Extract the stock name from the JSON object
+                QString stockName = jsonObject["symbol"].toString();
+                std::cout << "Stock Name: " << stockName.toStdString() << std::endl;
+
+                // Construct JSON object
+                QJsonObject messageObject;
+                messageObject["stockName"] = stockName;
+                messageObject["highValue"] = highValue;
+                //messageObject["responseData"] = QString::fromUtf8(responseData);
+
+                // Convert JSON object to string
+                QJsonDocument doc(messageObject);
+                QByteArray jsonBytes = doc.toJson(QJsonDocument::Compact);
+                std::string jsonString = "thibe>quest!>" +jsonBytes.toStdString();
+
+                // Send message back to the subscriber
+                ventilator.send(zmq::buffer(jsonString), zmq::send_flags::dontwait);
+            }
+            else {
+                std::cout << "Making the second API request..." << std::endl;
+
+                //Make the second API request
+                QNetworkRequest networkRequesttwee(QUrl("https://api.polygon.io/v1/open-close/AAPL/2023-01-09?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS"));
+                QNetworkReply* replytwee = manager.get(networkRequesttwee);
+
+                loop.exec();
                 if (reply->error() == QNetworkReply::NoError) {
-                    QByteArray responseData = reply->readAll();
-                    ventilator.send(responseData.constData(), responseData.size());
-                } else {
-                    std::cerr << "Network request failed: " << reply->errorString().toStdString() << std::endl;
-                }
-                reply->deleteLater();
-            });
-            return a.exec();
-        }
-        else if (receivedMessage == StockAAPL)
-        {
-            // Send the appropriate message to the ventilator
-            ventilator.send("Stock>quest?>AAPL", 17);
-            // Make the HTTP request
-            QNetworkAccessManager networkManager;
-            QNetworkRequest request(QUrl("https://api.polygon.io/v1/open-close/AAPL/2023-01-09?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS"));
-            QNetworkReply *reply = networkManager.get(request);
+                    // Read API response
+                    QByteArray responseDatatwee = replytwee->readAll();
+                    QJsonDocument jsonResponsetwee = QJsonDocument::fromJson(responseDatatwee);
+                    QJsonObject jsonObjecttwee = jsonResponsetwee.object();
 
-            // Connect to the finished signal of the network reply
-            QObject::connect(reply, &QNetworkReply::finished, [=, &ventilator]() {
-                if (reply->error() == QNetworkReply::NoError) {
-                    QByteArray responseData = reply->readAll();
-                    ventilator.send(responseData.constData(), responseData.size());
-                } else {
-                    std::cerr << "Network request failed: " << reply->errorString().toStdString() << std::endl;
-                }
-                reply->deleteLater();
-            });
-            return a.exec();
-        }
-        else if (receivedMessage == "Stock>quest?>CADLPrevious")
-        {
-            // Send the appropriate message to the ventilator
-            ventilator.send("Stock>quest?>CADLPrevious", 25);
-            // Make the HTTP request
-            QNetworkAccessManager networkManager;
-            QNetworkRequest request(QUrl("https://api.polygon.io/v2/aggs/ticker/CADL/prev?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS"));
-            QNetworkReply *reply = networkManager.get(request);
+                    // Extract the "high" value from the JSON object
+                    double highValuetwee = jsonObjecttwee["high"].toDouble();
+                    std::cout << "High Value: " << highValuetwee << std::endl;
 
-            // Connect to the finished signal of the network reply
-            QObject::connect(reply, &QNetworkReply::finished, [=, &ventilator]() {
-                if (reply->error() == QNetworkReply::NoError) {
-                    QByteArray responseData = reply->readAll();
-                    ventilator.send(responseData.constData(), responseData.size());
-                } else {
-                    std::cerr << "Network request failed: " << reply->errorString().toStdString() << std::endl;
-                }
-                reply->deleteLater();
-            });
-            return a.exec();
-        }
-        else if (receivedMessage == StockAAPLGROUPED)
-        {
-            // Send the appropriate message to the ventilator
-            ventilator.send("Stock>quest?>PreviousAAPL", 25);
-            // Make the HTTP request
-            QNetworkAccessManager networkManager;
-            QNetworkRequest request(QUrl("https://api.polygon.io/v2/aggs/ticker/AAPL/prev?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS"));
-            QNetworkReply *reply = networkManager.get(request);
+                    // Extract the stock name from the JSON object
+                    QString stockNametwee = jsonObjecttwee["symbol"].toString();
+                    std::cout << "Stock Name: " << stockNametwee.toStdString() << std::endl;
 
-            // Connect to the finished signal of the network reply
-            QObject::connect(reply, &QNetworkReply::finished, [=, &ventilator]() {
-                if (reply->error() == QNetworkReply::NoError) {
-                    QByteArray responseData = reply->readAll();
-                    ventilator.send(responseData.constData(), responseData.size());
-                } else {
-                    std::cerr << "Network request failed: " << reply->errorString().toStdString() << std::endl;
+                    QJsonObject messageObjecttwee;
+                    messageObjecttwee["stockName"] = stockNametwee;
+                    messageObjecttwee["highValue"] = highValuetwee;
+
+                    QJsonDocument doc(messageObjecttwee);
+                    QByteArray jsonBytes = doc.toJson(QJsonDocument::Compact);
+                    std::string jsonString = "thibe>quest!>" +jsonBytes.toStdString();
+
+                    ventilator.send(zmq::buffer(jsonString), zmq::send_flags::dontwait);
                 }
-                reply->deleteLater();
-            });
-            return a.exec();
+
+            }
+        } else {
+            std::cerr << "Failed to perform HTTP request: " << reply->errorString().toStdString() << std::endl;
         }
-    }
-    catch (zmq::error_t &ex)
-    {
-        std::cerr << "Caught an exception : " << ex.what();
+
+        reply->deleteLater();
+
+    } catch (const zmq::error_t& ex) {
+        std::cerr << "Caught an exception: " << ex.what() << std::endl;
     }
 
-    return 0;
+    return a.exec();
 }
--- Sender/Sender.cpp
+++ Sender/Sender.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 #include <string>
 #include <zmq.hpp>
-#ifndef _WIN32
-#include <unistd.h>
-#else
-#include <windows.h>
-#define sleep(n)    Sleep(n)
-#endif
 
-int main( void )
-{
-    try
-    {
+int main() {
+    try {
         zmq::context_t context(1);
 
-        //Outgoing message go out through here
-        zmq::socket_t ventilator( context, ZMQ_PUSH );
-        ventilator.connect( "tcp://benternet.pxl-ea-ict.be:24041" );
+        // Outgoing messages go out through here
+        zmq::socket_t ventilator(context, ZMQ_PUSH);
+        ventilator.connect("tcp://benternet.pxl-ea-ict.be:24041");
 
-        //Incoming messages come in here
-        zmq::socket_t subscriber( context, ZMQ_SUB );
-        subscriber.connect( "tcp://benternet.pxl-ea-ict.be:24042" );
-        subscriber.setsockopt( ZMQ_SUBSCRIBE, "Stock>quest?>", 13 );
+        // Incoming messages come in here
+        zmq::socket_t subscriber(context, ZMQ_SUB);
+        subscriber.connect("tcp://benternet.pxl-ea-ict.be:24042");
+        subscriber.setsockopt(ZMQ_SUBSCRIBE, "thibe>quest!>", 13); // Subscribe to all messages
 
-        zmq::message_t * msg = new zmq::message_t();
+        // Send a message
+        std::string message = "thibe>quest?>tijdd";
+        ventilator.send(zmq::buffer(message), zmq::send_flags::dontwait);
 
-        ventilator.send(  "Stock>quest?>AAPL", 17 );
-        subscriber.recv( msg );
-
-        std::cout << "Subscribed : [" << std::string( (char*) msg->data(), msg->size() ) << "]" << std::endl;
-
-    }
-    catch( zmq::error_t & ex )
-    {
-        std::cerr << "Caught an exception : " << ex.what();
+        // Receive response
+        while (true) {
+        zmq::message_t msg;
+        subscriber.recv(msg);
+        std::cout << "Received: " << std::string(static_cast<char*>(msg.data()), msg.size()) << std::endl;
+}
+    } catch (const zmq::error_t& ex) {
+        std::cerr << "Caught an exception: " << ex.what() << std::endl;
     }
 
     return 0;
 }
-//https://api.polygon.io/v1/open-close/CADL/2023-01-09?adjusted=true&apiKey=9R_AeMLkHBOPzgqDs2HiizXhCI4r67CS
